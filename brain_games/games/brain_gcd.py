836edabe3cfc53b405d0e1d7cba86a111168f5ff
Hayk <haykokarapetyan96@gmail.com>
2025-03-07 11:05:09 +0400

    fixed brain_gcd

--- brain_games/games/brain_gcd.py
+++ brain_games/games/brain_gcd.py
@@ -2,12 +2,10 @@ from random import randint
 
 from brain_games.scripts.game_engine import run_game
 
-
 def gcd(a, b):
     while b != 0:
         a, b = b, a % b
     return a
-
 
 def generate_question_and_answer():
     left_number = randint(1, 100)
@@ -15,7 +13,6 @@ def generate_question_and_answer():
     question = f"Question: {left_number} {right_number}"
     correct_answer = str(gcd(left_number, right_number))
     return question, correct_answer
-
 
 def main():
     game_rule = 'Find the greatest common divisor of given numbers.'
